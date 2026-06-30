089d79a5bf04b9f3908c12f1d5ad112df95aef4e
xGihyun <grdnnz.dev@proton.me>
2026-06-30 08:48:40 +0800

    feat(typography): add variants with cva

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -1,14 +1,41 @@
+import { cva, type VariantProps } from "class-variance-authority";
 import type { ComponentProps } from "react";
 import type { JSX } from "react/jsx-runtime";
 
 import { cn } from "@/lib/utils";
 
-export function H1(props: ComponentProps<"h1">): JSX.Element {
-	return (
-		<h1 className={cn("font-fira-mono-bold text-5xl", props.className)}>{props.children}</h1>
-	);
+const typography = cva("", {
+	variants: {
+		variant: {
+			h1: "font-fira-mono-bold text-5xl",
+			h2: "font-fira-mono-bold text-4xl",
+			p: "font-fira-mono text-base",
+		},
+	},
+});
+
+type TypographyProps = VariantProps<typeof typography>;
+
+export function H1({
+	variant = "h1",
+	className,
+	...props
+}: ComponentProps<"h1"> & TypographyProps): JSX.Element {
+	return <h1 className={cn(typography({ variant }), className)} {...props} />;
 }
 
-export function P(props: ComponentProps<"p">): JSX.Element {
-	return <p className={cn("font-fira-mono text-base", props.className)}>{props.children}</p>;
+export function H2({
+	variant = "h2",
+	className,
+	...props
+}: ComponentProps<"h2"> & TypographyProps): JSX.Element {
+	return <h2 className={cn(typography({ variant }), className)} {...props} />;
+}
+
+export function P({
+	variant = "p",
+	className,
+	...props
+}: ComponentProps<"p"> & TypographyProps): JSX.Element {
+	return <p className={cn(typography({ variant }), className)} {...props} />;
 }
