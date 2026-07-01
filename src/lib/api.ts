1187bf741d31f1575c1cb9dcbd06c354f3e7eb03
xGihyun <grdnnz.dev@proton.me>
2026-07-01 11:07:12 +0800

    chore: overload the response schema so `data` won't be nullable by default

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,15 @@ import * as v from "valibot";
 
 export type ApiResponse<T = null> = { message: string; data: T };
 
+export function ApiResponseSchema(): v.ObjectSchema<
+	{ message: v.StringSchema<undefined>; data: v.NullSchema<undefined> },
+	undefined
+>;
+
+export function ApiResponseSchema<T extends v.GenericSchema>(
+	dataSchema: T,
+): v.ObjectSchema<{ message: v.StringSchema<undefined>; data: T }, undefined>;
+
 export function ApiResponseSchema<T extends v.GenericSchema>(dataSchema?: T) {
 	return v.object({
 		message: v.string(),
